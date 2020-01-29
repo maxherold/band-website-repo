aca301609988fd39fcee7846d75ad7c748f49cba
Max Herold <49107849+maxherold@users.noreply.github.com>
2020-01-29 12:43:03 -0600

    git commit -m "Trying to add second webpage"

---
+++ Cynicicity.js
@@ -0,0 +1,9 @@
+ const $icon = document.querySelector(".icon");
+      const $arrow = document.querySelector(".arrow");
+
+      $icon.onclick = () => {
+        $arrow.animate([{ left: "0" }, { left: "12px" }, { left: "0" }], {
+          duration: 999
+          iterations: Infinity
+        });
+      };
