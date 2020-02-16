9fa71e61f4348dda41a897fb0ea06c2b796c140b
Max Herold <49107849+maxherold@users.noreply.github.com>
2020-02-15 21:48:55 -0600

    Add files via upload

--- Cynicicity.js
+++ Cynicicity.js
@@ -3,7 +3,7 @@
 
       $icon.onclick = () => {
         $arrow.animate([{ left: "0" }, { left: "12px" }, { left: "0" }], {
-          duration: 999
+          duration: 999,
           iterations: Infinity
         });
       };
