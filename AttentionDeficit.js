98f38684f67c2b4e0ed95d5e23e2d512e7b7e22d
Max Herold <49107849+maxherold@users.noreply.github.com>
2020-02-24 23:35:22 -0600

    Add files via upload

--- AttentionDeficit.js
+++ AttentionDeficit.js
@@ -1,6 +1,4 @@
-mongodb+srv //maxherold:<Allstar1234%24%21%24%21>@the-band-jpcoi.azure.mongodb.net/test?retryWrites=true&w=majority
-
- const $icon = document.querySelector(".icon");
+const $icon = document.querySelector(".icon");
       const $arrow = document.querySelector(".arrow");
 
       $icon.onclick = () => {
